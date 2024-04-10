c1e2ec862a309dc69e694516fa1213ca2525bf2c
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-11 00:40:53 +0100

    new commits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,53 @@
 import React from "react";
 import Link from "next/link";
 import Menu from "./Menu";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { IoIosArrowDropdown } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white h-20 w-full text-black flex flex-row px-3 md:flex-row items-center justify-between md:h-24 lg:px-20 xl:px-40">
-      {/* LEFT SIDE */}
-        <h1 className="text-2xl font-bold">Rotrade</h1>
-      <div className="hidden md:flex gap-4 flex-1 items-center justify-center">
-        {/* Links */}
-        <Link href="/">Home</Link>
-        <Link href="/services">Services</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
-      {/* Right Side - Visible on Small Screens */}
-      <div className="md:hidden flex flex-row justify-center items-center space-x-3 pt-5 pb-5">
-        <div className="md:hidden">
-          <Menu />
+   
+
+    <nav className="bg-gray-800 p-4 flex justify-between items-center">
+      {/* Left content */}
+      <div className="text-white">Logo</div>
+
+      {/* Right content */}
+      <div className="flex items-center">
+        {/* Cart */}
+        <div className="flex flex-row space-x-2 text-white mr-4">
+          <h6>Cart</h6>
+          <RiShoppingCartLine size={24} />
         </div>
-      </div>
-      {/* Right Side - Hidden on Small Screens */}
-      <div className="hidden md:flex items-center justify-center space-x-3">
-        <button className="bg-blue-700 text-white px-3 py-3 rounded hidden md:block">
-          7812691505
-        </button>
+
+        {/* Dropdown */}
+        <div className="relative">
+          <div className="text-white cursor-pointer">
+            <IoIosArrowDropdown size={24} />
+          </div>
+          <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg hidden">
+            <div className="py-1">
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Item 1
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Item 2
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+              >
+                Item 3
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
   );
