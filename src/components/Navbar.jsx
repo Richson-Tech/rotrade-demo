4736d63ee4ec2423542665a4f76a663692f44d6e
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-11 00:29:55 +0100

    new commits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white h-20 w-full text-black flex flex-row px-3 md:flex-row items-center justify-between md:h-24 lg:px-20 xl:px-40">
       {/* LEFT SIDE */}
-      <div className="flex flex-row ">
         <h1 className="text-2xl font-bold">Rotrade</h1>
-        <p className="text-[#757575] text-sm">COMMUNITY SERVICE BLOCK GRANT</p>
-      </div>
       <div className="hidden md:flex gap-4 flex-1 items-center justify-center">
         {/* Links */}
         <Link href="/">Home</Link>
