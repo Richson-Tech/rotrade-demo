b1d23acfd4475022cd36e1d031a533c6bcfd064e
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-11 01:01:27 +0100

    new commits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Link from "next/link";
 import Menu from "./Menu";
 import { RiShoppingCartLine } from "react-icons/ri";
-import { PiNumberCircleOneBold } from "react-icons/pi";
+import { MdExposureZero } from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -20,32 +19,8 @@ const Navbar = () => {
         </div>
 
         {/* Dropdown */}
-        <div className="relative">
-          <div className="text-white cursor-pointer">
-            <IoIosArrowDropdown size={24} />
-          </div>
-          <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg hidden">
-            <div className="py-1">
-              <a
-                href="#"
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-              >
-                Item 1
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-              >
-                Item 2
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
-              >
-                Item 3
-              </a>
-            </div>
-          </div>
+        <div>
+         <Menu/>
         </div>
       </div>
     </nav>
