0d75e6b4643a29e383b730afce65cf4b0f4fd960
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-11 01:37:02 +0100

    new commits

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,55 +1,3 @@
-// "use client";
-
-// import React, { useState } from "react";
-// import Link from "next/link";
-// import { MdOutlineClose } from "react-icons/md";
-// import { IoIosArrowForward, IoIosArrowRoundForward } from "react-icons/io";
-// import { IoIosArrowDropdown } from "react-icons/io";
-
-// const links = [
-//   { id: 1, title: "Home", url: "/" },
-//   { id: 2, title: "Services", url: "/services" },
-//   { id: 3, title: "About", url: "/about" },
-//   { id: 4, title: "Contact", url: "/contact" },
-// ];
-
-// const Menu = () => {
-//   const [open, setOpen] = useState(false);
-
-//   return (
-//     <nav>
-//       {open ? (
-//         <MdOutlineClose
-//           className="text-white"
-//           size="2rem"
-//           onClick={() => setOpen(false)}
-//         />
-//       ) : (
-//         <IoIosArrowDropdown
-//           className="text-white"
-//           size="2rem"
-//           onClick={() => setOpen(true)}
-//         />
-//       )}
-
-//       {open && (
-//         <div className="bg-blue-700 text-black absolute left-0 top-24 w-full mt-10 h-96 flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10">
-//           {links.map((item) => (
-//             <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
-//               <div className="flex items-center justify-between">
-//                 <span>{item.title}</span>
-//                 <IoIosArrowRoundForward size={24} />
-//               </div>
-//             </Link>
-//           ))}
-//         </div>
-//       )}
-//     </nav>
-//   );
-// };
-
-// export default Menu;
-
 "use client";
 
 import React, { useState } from "react";
@@ -89,7 +37,7 @@ const Menu = () => {
           {links.map((item) => (
             <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
               <div className="flex items-center justify-between">
-                <span>{item.title}</span>
+                <span className="text-lg font-serif">{item.title}</span>
                 <IoIosArrowRoundForward className="ml-4" size={24} />
               </div>
             </Link>
