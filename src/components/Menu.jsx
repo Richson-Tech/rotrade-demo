fd6047a581ac944c51e21a040dd27fc5123a4ee2
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-04-11 13:45:35 +0100

    new commits

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -33,7 +33,7 @@ const Menu = () => {
       )}
 
       {open && (
-        <div className="bg-blue-700 text-black absolute left-0 top-24 w-full mt-10 h-96 flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10">
+        <div className="bg-blue-700 text-black absolute left-0 top-24 w-full mt-10 h-96 flex flex-col gap-8 pl-6 items-start justify-center text-3xl font-serif2 z-10 origin-center">
           {links.map((item) => (
             <Link key={item.id} href={item.url} onClick={() => setOpen(false)}>
               <div className="flex items-center justify-between">
